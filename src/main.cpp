6fe671968e3a675b1f6a6768bec7783fc499d769
kevin <kevinxdoran@gmail.com>
2018-09-17 11:48:50 +1200

    init. Qt is building.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+    QMainWindow w;
+    w.show();
+    return a.exec();
+}
