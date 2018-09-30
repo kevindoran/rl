8e4dde66f22466618d1a67d02c4af83c56922933
kevin <kevinxdoran@gmail.com>
2018-10-01 09:18:07 +1300

    DeterministicPolicy now stores copies of States and Actions instead of references.

--- src/core/DeterministicPolicy.h
+++ src/core/DeterministicPolicy.h
@@ -26,10 +26,20 @@ public:
     }
 
 private:
-    // We could just have a map from ID->ID. State->Action, however, allows for easy reporting
-    // and debugging. Hmm... but using the references makes everything difficult to copy.
-    using StateToActionMap = std::unordered_map<std::reference_wrapper<State>,
-                                                std::reference_wrapper<Action>>;
+    // Options for this map:
+    //    1. ID->ID.
+    //    2. State->Action
+    //    3. State&->Action&
+    //
+    //    Benefits of #1 & #2: we are not tying this policy object to a specific environment object.
+    //    #3 will tie the policy object to a specific environment.
+    //    Benefits of #1 & #3: we only store a single 32 or 64 bit value.
+    //    Beneifts of #2: we get very nice debugging capabilities.
+    //    For the moment, #2 will be used, and we may switch to #1 if we want better performance.
+    //    #3 shouldn't be used, as we would like a single policy to work on multiple Environments,
+    //    and we do not wish to restrict the implementation of Environment such that all
+    //    Environments use the same state, action and policy objects.
+    using StateToActionMap = std::unordered_map<State, Action>;
     StateToActionMap state_to_action_{};
 };
 
