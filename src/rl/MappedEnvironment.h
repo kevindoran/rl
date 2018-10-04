dc972e5414c41e3a4cd28535e9c359eb5fdd5df8
kevin <kevinxdoran@gmail.com>
2018-10-04 15:50:00 +1300

    Moving some of the functionality of MappedEnvironment into rl::impl::Environment.
    Along the way, some SEGFAULTS highlight the lack of some nullptr checks. Added these missing ones.

--- src/rl/MappedEnvironment.h
+++ src/rl/MappedEnvironment.h
@@ -12,6 +12,7 @@
 #include <glog/logging.h>
 
 #include "rl/Environment.h"
+#include "rl/impl/Environment.h"
 #include "rl/DistributionTree.h"
 
 
@@ -41,7 +42,7 @@ struct cumulative_grouping_less {
     }
 };
 
-class MappedEnvironment : public Environment {
+class MappedEnvironment : public impl::Environment {
 public:
 
     // https://stackoverflow.com/questions/32040426/expose-c-container-iterator-to-user
@@ -124,55 +125,10 @@ public:
         return random_transition;
     }
 
-    ID state_count() const override {
-        return static_cast<ID>(states_.size());
-    }
-
-    const State& current_state() const {
-        return *states_[current_state_];
-    }
-
-    State& current_state() {
-        return const_cast<State&>(static_cast<const MappedEnvironment*>(this)->current_state());
-    }
-
-    const State& state(ID id) const {
-        return *states_[id];
-    }
-
-    State& state(ID id) {
-        return const_cast<State&>(static_cast<const MappedEnvironment*>(this)->state(id));
-    }
-
-
-    const Action& action(ID id) const {
-        return *actions_[id];
-    }
-
-    Action& action(ID id) {
-        return const_cast<Action&>(static_cast<const MappedEnvironment*>(this)->action(id));
-    }
-
-    const Reward& reward(ID id) const {
-        return *rewards_[id];
-    }
-
-    Reward& reward(ID id) {
-        return const_cast<Reward&>(static_cast<const MappedEnvironment*>(this)->reward(id));
-    }
-
     void set_all_rewards_to(double value) {
         for(auto& p_reward : rewards_) {
             p_reward->set_value(value);
         }
-    }
-
-    double accumulated_reward() {
-        return accumulated_reward_;
-    }
-
-    bool is_in_end_state() {
-        return end_states_.find(current_state_) != std::end(end_states_);
     }
 
     void set_start_state(const State& state) {
@@ -182,57 +138,10 @@ public:
         }
     }
 
-    void restart() {
+    void restart() override {
         current_state_ = start_state_;
         accumulated_reward_ = 0;
         started_ = false;
-    }
-
-    std::vector<std::reference_wrapper<const State>> end_states() {
-        std::vector<std::reference_wrapper<const State>> ans;
-        std::transform(end_states_.begin(), end_states_.end(), ans.begin(),
-            [this](ID id) {
-                return std::cref(state(id));
-            });
-        return ans;
-    }
-
-    bool is_end_state(const State& s) const {
-        auto finder = std::find(end_states_.begin(), end_states_.end(), s.id());
-        bool is_an_end_state = finder != std::end(end_states_);
-        return is_an_end_state;
-    }
-
-    StateIterator states_begin() const {
-        return util::DereferenceIterator(states_.cbegin());
-    }
-
-    StateIterator states_end() const {
-        return util::DereferenceIterator(states_.cend());
-    }
-
-    States states() const {
-        return States(states_begin(), states_end());
-    }
-
-    ActionIterator actions_begin() const {
-        return util::DereferenceIterator(actions_.cbegin());
-    }
-
-    ActionIterator actions_end() const {
-        return util::DereferenceIterator(actions_.cend());
-    }
-
-    Actions actions() const {
-        return Actions(actions_begin(), actions_end());
-    }
-
-    RewardIterator rewards_begin() const {
-        return util::DereferenceIterator(rewards_.cbegin());
-    }
-
-    RewardIterator rewards_end() const {
-        return util::DereferenceIterator(rewards_.cend());
     }
 
     void build_distribution_tree() {
@@ -308,84 +217,13 @@ private:
         return get_dist_node(state, action, next_state).child_with_id(reward);
     }
 
-    /*
-    void build_distribution_tree() {
-        // Clear the tree so we can call this multiple times.
-        dist_tree_ = std::move(DistributionTree<Transition>());
-        // std::sort only works with random access iterators.
-        // std::sort(std::begin(transitions_), std::end(transitions_), cumulative_grouping_less());
-        transitions_.sort(cumulative_grouping_less());
-        const auto transition_it{std::begin(transitions_)};
-        DistNode& root = dist_tree_.root_node();
-        int added_count = 0;
-        for(const std::unique_ptr<State>& s : states_) {
-            // We should never have a transition that applies to a loop we have already covered.
-            Ensures(transition_it->state() >= s->id());
-            if(transition_it->state() > s->id()) {
-                continue;
-            }
-            DistNode& state_node = root.add_child();
-            Ensures(&state_node == &root.child(s->id()));
-            for(const std::unique_ptr<Action>& a : actions_) {
-                Ensures(transition_it->action() >= a->id());
-                if(transition_it->action() > a->id()) {
-                    continue;
-                }
-                DistNode& action_node = state_node.add_child();
-                for(const std::unique_ptr<State>& next_state : states_) {
-                    Ensures(transition_it->next_state() >= next_state->id());
-                    if(transition_it->next_state() > next_state->id()) {
-                        continue;
-                    }
-                    DistNode& next_state_node = action_node.add_child();
-                    for(const std::unique_ptr<Reward>& reward : rewards_) {
-                        Ensures(transition_it->reward() >= reward->id());
-                        if(transition_it->reward() > reward->id()) {
-                            continue;
-                        }
-                        Transition& matching_transition{*transition_it};
-                        std::next(transition_it);
-                        // Double check everything is correct:
-                        // We should have continued looping if this wasn't a perfect match.
-                        Ensures(s->id() == matching_transition.state());
-                        Ensures(a->id() == matching_transition.action());
-                        Ensures(next_state->id() == matching_transition.next_state());
-                        Ensures(reward->id() == matching_transition.reward());
-
-                        next_state_node.add_child(matching_transition.prob_weight(),
-                                                  &matching_transition);
-                        added_count++;
-                    }
-                }
-            }
-        }
-        // Make sure we have added all of the transitions to the distribution tree.
-        Ensures(transitions_.size() <= std::numeric_limits<ID>::max());
-        Ensures(added_count == static_cast<ID>(transitions_.size()));
-        dist_tree_.update_weights();
-    }
-    */
-
 private:
     DistTree dist_tree_;
-    ID current_state_ = 0;
     bool started_ = false;
     ID start_state_ = 0;
-    double accumulated_reward_ = 0;
     bool needs_rebuilding_ = false;
 
-    /* Using list vs vector.
-     * We wish to be able to hold references/pointers to elements of the below containers. As the
-     * locations of vector entries may move, we would need to use std::vector<std::unique_ptr<Type>>
-     * to store elements in a vector. To make things a little simplier we could instead use a list.
-     * The list guarantees the stable location of its elements on the heap.
-     */
-    std::vector<std::unique_ptr<State>> states_{};
-    std::unordered_set<ID> end_states_{};
-    std::vector<std::unique_ptr<Action>> actions_{};
-    std::vector<std::unique_ptr<Reward>> rewards_{};
     std::set<Transition, cumulative_grouping_less> transitions_{};
-
 };
 
 } // namespace rl
--- src/rl/impl/Environment.h
+++ src/rl/impl/Environment.h
@@ -1,4 +1,156 @@
 #pragma once
 
+#include <gsl/gsl>
+#include <glog/logging.h>
+#include <vector>
+#include <unordered_set>
+#include <memory>
+
 #include "rl/Environment.h"
+#include "util/DereferenceIterator.h"
+#include "util/RangeWrapper.h"
+
+
+namespace rl {
+namespace impl {
+class Environment;
+} // namespace impl
+
+// This idea for naming came from Core Guidelines C.129. It seems a
+// little hacky though, for example: not being able to define the class within the nested namespace.
+// I'll leave it like this for the moment and see what it's like to use.
+
+/**
+ * Inheritance base class providing common method implementations for Environment.
+ *
+ * This class is virtual and can only exist as a base class of an object. Thus, I have marked all
+ * constructors as protected.
+ */
+class impl::Environment : public rl::Environment {
+
+protected:
+    Environment() = default;
+    // Can't have copy unless we manually treat out vector of unique pointers.
+    Environment(const Environment&) = delete;
+    Environment& operator=(const Environment&) = delete;
+    // Let's see if we can do move correctly with an inheritance hierachy.
+    Environment(Environment&&) = default;
+    Environment& operator=(Environment&&) = default;
+    // Protected and non-virtual destructor. Following reasoning from Core Guidelines:
+    // http://www.modernescpp.com/index.php/c-core-guidelines-destructor-rules
+    ~Environment() = default;
+
+public:
+    ID state_count() const override {
+        return static_cast<ID>(states_.size());
+    }
+
+    const State& current_state() const override {
+        Ensures(current_state_ < static_cast<ID>(states_.size()));
+        return *CHECK_NOTNULL(states_[current_state_]);
+    }
+
+    State& current_state() override {
+        return const_cast<State&>(static_cast<const impl::Environment*>(this)->current_state());
+    }
+
+    const State& state(ID id) const override {
+        Expects(id < static_cast<ID>(states_.size()));
+        return *CHECK_NOTNULL(states_[id]);
+    }
+
+    State& state(ID id) override {
+        return const_cast<State&>(static_cast<const impl::Environment*>(this)->state(id));
+    }
+
+    const Action& action(ID id) const override {
+        Expects(id < static_cast<ID>(actions_.size()));
+        return *CHECK_NOTNULL(actions_[id]);
+    }
+
+    Action& action(ID id) override {
+        return const_cast<Action&>(static_cast<const impl::Environment*>(this)->action(id));
+    }
+
+    const Reward& reward(ID id) const override {
+        Expects(id < static_cast<ID>(rewards_.size()));
+        return *CHECK_NOTNULL(rewards_[id]);
+    }
+
+    Reward& reward(ID id) override {
+        return const_cast<Reward&>(static_cast<const impl::Environment*>(this)->reward(id));
+    }
+
+    double accumulated_reward() override {
+        return accumulated_reward_;
+    }
+
+    bool is_in_end_state() override {
+        return end_states_.find(current_state_) != std::end(end_states_);
+    }
+
+    std::vector<std::reference_wrapper<const State>> end_states() override {
+        std::vector<std::reference_wrapper<const State>> ans;
+        std::transform(end_states_.begin(), end_states_.end(), ans.begin(),
+                       [this](ID id) {
+                           return std::cref(state(id));
+                       });
+        return ans;
+    }
+
+    bool is_end_state(const State& s) const override {
+        auto finder = std::find(end_states_.begin(), end_states_.end(), s.id());
+        bool is_an_end_state = finder != std::end(end_states_);
+        return is_an_end_state;
+    }
+
+    StateIterator states_begin() const override {
+        return util::DereferenceIterator(states_.cbegin());
+    }
+
+    StateIterator states_end() const override {
+        return util::DereferenceIterator(states_.cend());
+    }
+
+    States states() const override {
+        return States(states_begin(), states_end());
+    }
+
+    ActionIterator actions_begin() const override {
+        return util::DereferenceIterator(actions_.cbegin());
+    }
+
+    ActionIterator actions_end() const override {
+        return util::DereferenceIterator(actions_.cend());
+    }
+
+    Actions actions() const override {
+        return Actions(actions_begin(), actions_end());
+    }
+
+    RewardIterator rewards_begin() const override {
+        return util::DereferenceIterator(rewards_.cbegin());
+    }
+
+    RewardIterator rewards_end() const override {
+        return util::DereferenceIterator(rewards_.cend());
+    }
+
+protected:
+    ID current_state_ = 0;
+    double accumulated_reward_ = 0;
+
+    /* Using list vs vector.
+     * We wish to be able to hold references/pointers to elements of the below containers. As the
+     * locations of vector entries may move, we would need to use std::vector<std::unique_ptr<Type>>
+     * to store elements in a vector. To make things a little simplier we could instead use a list.
+     * The list guarantees the stable location of its elements on the heap.
+     */
+    std::vector<std::unique_ptr<State>> states_{};
+    std::unordered_set<ID> end_states_{};
+    std::vector<std::unique_ptr<Action>> actions_{};
+    std::vector<std::unique_ptr<Reward>> rewards_{};
+};
+
+} // namespace rl
 
