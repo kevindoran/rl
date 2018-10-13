cffaae6cba95aa646fcdd754326c5eee2383951c
kevin <kevinxdoran@gmail.com>
2018-10-13 15:57:49 +1300

    Moving two methods from header to cpp file.

---
+++ src/rl/Policy.cpp
@@ -0,0 +1,27 @@
+#include "rl/Policy.h"
+
+namespace rl {
+
+double error_as_factor(double prev, double updated) {
+    double abs_error = std::abs(updated - prev);
+    if(abs_error == 0) {
+        return 0;
+    }
+    // We will be conservative about assuming which is more accurate, prev and updated, and
+    // choose the smaller one, which will produce the largest error.
+    double error_as_factor = 1;
+    double denom = std::min(std::abs(prev), std::abs(updated));
+    if(denom != 0) {
+        error_as_factor = abs_error / denom;
+    }
+    return error_as_factor;
+}
+
+int compare(double val1, double val2, double error_factor) {
+    if(error_as_factor(val1, val2) <= error_factor) {
+        return 0;
+    }
+    return (val1 > val2) ? 1 : -1;
+}
+
+} // namespace rl
--- src/rl/Policy.h
+++ src/rl/Policy.h
@@ -9,7 +9,9 @@
 
 namespace rl {
 
+double error_as_factor(double prev, double updated);
 
+int compare(double val1, double val2, double error_factor);
 
 /**
  * For every state in an Environment, Policy defines actions to be taken (with probability).
@@ -68,38 +70,9 @@ public:
     virtual ActionDistribution possible_actions(const Environment& e,
                                                 const State& from_state) const = 0;
 
-    // Should we make this pure? If it is not pure, Policy might not be considered a virtual class.
-    // Is there issues with that?
     virtual ~Policy() = default;
 };
 
-// TODO: edit the methods evaluate() and improve() to accept some options that allow stopping
-// conditions to be controlled. It would also be nice to be able to interact with the in-progress
-// results after a certain number of loops or units of time.
-
-
-// TODO: move to .cc
-inline double error_as_factor(double prev, double updated) {
-    double abs_error = std::abs(updated - prev);
-    if(abs_error == 0) {
-        return 0;
-    }
-    // We will be conservative about assuming which is more accurate, prev and updated, and
-    // choose the smaller one, which will produce the largest error.
-    double error_as_factor = 1;
-    double denom = std::min(std::abs(prev), std::abs(updated));
-    if(denom != 0) {
-        error_as_factor = abs_error / denom;
-    }
-    return error_as_factor;
-}
-
-inline int compare(double val1, double val2, double error_factor) {
-    if(error_as_factor(val1, val2) <= error_factor) {
-        return 0;
-    }
-    return (val1 > val2) ? 1 : -1;
-}
 
 /**
  * Calculates the (state) value function for a Policy.
