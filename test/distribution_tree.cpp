ff36d53768580075c84d0995f6899be793df0c4f
kevin <kevinxdoran@gmail.com>
2018-10-20 16:45:09 +1300

    Made sure to initialize glog when running the tests.

--- test/distribution_tree.cpp
+++ test/distribution_tree.cpp
@@ -230,6 +230,6 @@ TEST(DistributionTreeTest, test_random_child) {
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    //google::InitGoogleLogging(argv[0]);
+    google::InitGoogleLogging(argv[0]);
     return RUN_ALL_TESTS();
 }
